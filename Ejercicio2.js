4cd94a9448b2d4d84f45f08962119c9221839070
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-02-27 14:38:09 -0500

    subiendo la tarea 3

---
+++ Ejercicio2.js
@@ -0,0 +1,11 @@
+let nombre = "Alessandro"
+let edad = 28
+let desarrollador = true
+let fechaNacimiento = new Date("may 25 1994")
+
+const libroFavorito={
+    titulo:"Harry potter y le principe mestizo",
+    autor:"J. K. Rowling",
+    fechaPublicacion:"2006",
+    url:"https://es.wikipedia.org/wiki/Harry_Potter_y_el_misterio_del_pr%C3%ADncipe_(pel%C3%ADcula)"
+}
