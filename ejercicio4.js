0cfe4ef0981124b4467d117a930a8ca933b24a1a
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-03-14 11:59:43 -0500

    tarea 3 cadenas

---
+++ ejercicio4.js
@@ -0,0 +1,14 @@
+let nombre="Alessandro"
+let apellido="Cisneros"
+let estudiante =`${nombre} ${apellido}`
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus=estudiante.toLowerCase()
+let numeroCaracter=estudiante.length
+let primeraLetra = nombre.charAt(0)
+let ultimaLetra = apellido[apellido.length-1]
+let sinespacios= estudiante.trim()
+let incluye = estudiante.includes(nombre)
+
+console.log(incluye)
+
+
