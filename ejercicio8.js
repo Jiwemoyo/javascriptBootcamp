8e7c45adfa582bd81a61168c90c82c8175483049
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-04-04 10:42:47 -0500

    ejercicio  modulo 8

---
+++ ejercicio8.js
@@ -0,0 +1,27 @@
+function verdadera() {
+    return true
+}
+
+async function holaPromesa() {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log("Hola soy una promesa");
+  }
+  
+  holaPromesa();
+
+
+  function* pares() {
+    let index = 0;
+    while (true) {
+      yield index;
+      index += 2;
+    }
+  }
+
+
+const generador = pares();
+console.log(generador.next().value); // 0
+console.log(generador.next().value); // 2
+console.log(generador.next().value); // 4
+console.log(generador.next().value); // 6
+console.log(generador.next().value); // 8
