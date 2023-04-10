a50910b3642ac57baea3dbba987d86356661e518
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-04-10 14:57:20 -0500

    ejercicio  modulo 11

---
+++ Ejercicio11.js
@@ -0,0 +1,16 @@
+class Estudiante {
+    constructor(nombre) {
+      this.nombre = nombre;
+      this.asignaturas = ['Javascript', 'HTML', 'CSS'];
+    }
+    
+    obtenDatos() {
+      return {
+        nombre: this.nombre,
+        asignaturas: this.asignaturas
+      };
+    }
+  }
+  
+  const estudiante1 = new Estudiante('Juan');
+  console.log(estudiante1.obtenDatos());
